58829efb0b87bb53a200b3f46fb511320725946d
zhangzchwn <1064494254@qq.com>
2020-08-12 07:12:15 +0800

    fix comment

---
+++ ux/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "message"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
